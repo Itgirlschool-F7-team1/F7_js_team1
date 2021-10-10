43833dd7aeb7e45758ca57d7f6c347b7e57ad9cf
JBatsyus <jkalabina@gmail.com>
2021-10-10 22:49:05 +0200

    Изменения

--- assets/scripts/bundle_motion_d.js
+++ assets/scripts/bundle_motion_d.js
@@ -4,37 +4,57 @@ const Chart = require('chart.js');
 // const moment = require('moment');
 // require('moment/locale/ru.js');
 
-
-
+// объявляю переменную с кнопкой
 let btn = document.getElementById('btn');
-// создаем массивы для данных
-let arrDate = [];
-let arrSteps = [];
-// проверяем, есть ли что-нибудь в lS
-document.addEventListener("DOMContentLoaded", function (event) {
-    if (localStorage.getItem('stepsToday') != null) {
-        arrDate = JSON.parse(localStorage.getItem('stepsToday'));
-    }
-    if (localStorage.getItem('steps') != null) {
-        arrSteps = JSON.parse(localStorage.getItem('steps'));
-    }
+// вешаю функцию на клик
+btn.addEventListener('click', function saveInfo() {
+        // Разбираем любой JSON, ранее сохраненный в allEntries
+    // getItem , чтобы получить старый список, добавить к нему, а затем сохранить его обратно в localStorage:
+            let existingEntries = JSON.parse(localStorage.getItem("allEntries"));
+            if (existingEntries == null) existingEntries = [];
+
+            const stepsToday = document.getElementById('localdate').value;
+            const steps = document.getElementById('steps_today').value;
+
+            let entry = {
+                "date": stepsToday,
+                "step": steps
+            };
+            localStorage.setItem("entry", JSON.stringify(entry));
+            // Сохраняю все записи обратно в локальное хранилище
+            existingEntries.push(entry);
+            localStorage.setItem("allEntries", JSON.stringify(existingEntries));
+
+// график
+    
+    
+let ctx = document.getElementById('myChart').getContext('2d');
+const data = entry;
+let chart = new Chart(ctx, {
+// Тип графика
+type: 'line',
+ 
+// Создание графиков
+data: {
+    // Точки графиков
+    labels: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль'],
+    // График
+    datasets: [{
+        label: 'Возможно это шагомер', // Название
+        backgroundColor: 'rgb(255, 99, 132)', // Цвет закраски
+        borderColor: 'rgb(255, 99, 132)', // Цвет линии
+        data: data// Данные каждой точки графика
+    }]
+},
+ 
+// Настройки графиков
+options: {}
+});
 });
 
-btn.addEventListener('click', function saveInfo() {
-    let stepsToday = document.getElementById('localdate').value;
-    // console.log(stepsToday);
-    let steps = document.getElementById('steps_today').value;
-    // console.log(steps);
 
 
-    arrDate.push(stepsToday);
-    localStorage.setItem('stepsToday', JSON.stringify(arrDate));
-    // console.log(arrDate);
-    arrSteps.push(steps);
-    localStorage.setItem('steps', JSON.stringify(arrSteps));
-    // console.log(arrSteps);
 
-})
 },{"chart.js":2}],2:[function(require,module,exports){
 /*!
  * Chart.js v3.5.1
