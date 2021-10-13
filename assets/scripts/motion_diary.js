5e65f251acae9bec33cfa09b300f9769e8db0f1e
JBatsyus <ju.kalabina@gmail.com>
2021-10-13 14:34:24 +0200

    Добавлены стили CSS

--- assets/scripts/motion_diary.js
+++ assets/scripts/motion_diary.js
@@ -3,53 +3,93 @@ const Chart = require('chart.js');
 // const moment = require('moment');
 // require('moment/locale/ru.js');
 
-// объявляю переменную с кнопкой
+
+// Вообще другой вариант
+
+
+
+// создаем массивы для данных
+let arrDate = [];
+let arrSteps = [];
+// проверяем, есть ли что-нибудь в lS
+document.addEventListener("DOMContentLoaded", function (event) {
+    if (localStorage.getItem('stepsToday') != null) {
+        arrDate = JSON.parse(localStorage.getItem('stepsToday'));
+    }
+    if (localStorage.getItem('steps') != null) {
+        arrSteps = JSON.parse(localStorage.getItem('steps'));
+    }
+});
+
+
 let btn = document.getElementById('btn');
-// вешаю функцию на клик
-btn.addEventListener('click', function saveInfo() {
-        // Разбираем любой JSON, ранее сохраненный в allEntries
-    // getItem , чтобы получить старый список, добавить к нему, а затем сохранить его обратно в localStorage:
-            let existingEntries = JSON.parse(localStorage.getItem("allEntries"));
-            if (existingEntries == null) existingEntries = [];
+btn.addEventListener('click', saveInfo);
+// btn.addEventListener('click', chartUpdate);
 
-            const stepsToday = document.getElementById('localdate').value;
-            const steps = document.getElementById('steps_today').value;
 
-            let entry = {
-                "date": stepsToday,
-                "step": steps
-            };
-            localStorage.setItem("entry", JSON.stringify(entry));
-            // Сохраняю все записи обратно в локальное хранилище
-            existingEntries.push(entry);
-            localStorage.setItem("allEntries", JSON.stringify(existingEntries));
+function saveInfo() {
+    let stepsToday = document.getElementById('localdate').value;
+    // console.log(stepsToday);
+    let steps = document.getElementById('steps_today').value;
+    // console.log(steps);
+
+
+    arrDate.push(stepsToday);
+    localStorage.setItem('stepsToday', JSON.stringify(arrDate));
+    // console.log(arrDate);
+    arrSteps.push(steps);
+    localStorage.setItem('steps', JSON.stringify(arrSteps));
+    // console.log(arrSteps);
+
+
 
 // график
-    
-    
-let ctx = document.getElementById('myChart').getContext('2d');
-const data = entry;
-let chart = new Chart(ctx, {
-// Тип графика
-type: 'line',
- 
-// Создание графиков
-data: {
-    // Точки графиков
-    labels: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль'],
-    // График
-    datasets: [{
-        label: 'Возможно это шагомер', // Название
-        backgroundColor: 'rgb(255, 99, 132)', // Цвет закраски
-        borderColor: 'rgb(255, 99, 132)', // Цвет линии
-        data: data// Данные каждой точки графика
-    }]
-},
- 
-// Настройки графиков
-options: {}
-});
-});
+    let labels = arrDate;
+    let data = arrSteps;
+    let colors = ['#b1a28d'];
+
+    let myChart = document.getElementById("myChart").getContext('2d');
+  
+    let chart = new Chart(myChart, {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Количество шагов',
+                borderWidth: 2,
+                fill: true,
+                backgroundColor: '#bf9999',
+                borderColor:'#fff',
+                data: data,
+                pointRadius: 6,
+                pointBackgroundColor:colors 
+                
+            }]
+        },
+        options: {
+
+            animations: {
+                tension: {
+                    duration: 1000,
+                    easing: 'linear',
+                    from: 1,
+                    to: 0,
+                    loop: true
+                }
+            },
+            scales: {
+                y: { // defining min and max so hiding the dataset does not change scale range
+                    min: 0,
+                    max: 15000
+                }
+            }
+
+        },
+
+    })
+   
+
+}
 
 
 
