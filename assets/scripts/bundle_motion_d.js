3ebcc120c313fe547784ca8468e6f8ec80287ae9
JBatsyus <jkalabina@gmail.com>
2021-10-14 11:22:49 +0200

    Разбор кода с тимлидом

--- assets/scripts/bundle_motion_d.js
+++ assets/scripts/bundle_motion_d.js
@@ -5,126 +5,209 @@
 
 const Chart = require('chart.js');
 
+// создаем класс, который будет формировать объект
+class CounterS {
+    constructor(today, steps) {
+        this.today = today;
+        this.steps = steps;
+    }
+}
 
-
-
-
-
-
-// создаем массивы для данных
+// создаем глобальные переменные с массивами
 let arrDate = [];
 let arrSteps = [];
 
-
-
-// проверяем, есть ли что-нибудь в lS
-document.addEventListener("DOMContentLoaded", function (event) {
-    if (localStorage.getItem('stepsToday') != null) {
-        arrDate = JSON.parse(localStorage.getItem('stepsToday'));
-    }
-    if (localStorage.getItem('steps') != null) {
-        arrSteps = JSON.parse(localStorage.getItem('steps'));
-    }
-});
-
-
-let btnMotionDiary = document.getElementById('btn_md');
-btnMotionDiary.addEventListener('click', saveInfo);
-btnMotionDiary.addEventListener('click', chartUpdate);
-
-
-
-function saveInfo() {
-    let stepsToday = document.getElementById('localdate').value;
-    // console.log(stepsToday);
-    let steps = document.getElementById('steps_today').value;
-    // console.log(steps);
-
-
-    arrDate.push(stepsToday);
-    localStorage.setItem('stepsToday', JSON.stringify(arrDate));
-    // console.log(arrDate);
-    arrSteps.push(steps);
-    localStorage.setItem('steps', JSON.stringify(arrSteps));
-    // console.log(arrSteps);
-
-
-
-    // график
-
-
-    let labelsMotionDiary = arrDate;
-    let dataMotionDiary = arrSteps;
-    let colorsMotionDiary = ['#b1a28d'];
-
-    let myChartMotionDiary = document.getElementById("myChartMotionDiary").getContext('2d');
-
-    myChartMotionDiary.canvas.width = 1200;
-    myChartMotionDiary.canvas.height = 350;
-
-    let chartMotionDiary = new Chart(myChartMotionDiary, {
-        type: 'line',
-        data: {
-            labels: labelsMotionDiary,
-            datasets: [{
-                label: 'Количество шагов',
-                borderWidth: 2,
-                fill: true,
-                backgroundColor: '#bf9999',
-                borderColor: '#fff',
-                data: dataMotionDiary,
-                pointRadius: 6,
-                pointBackgroundColor: colorsMotionDiary
-
-            }]
-        },
-        options: {
-
-            animations: {
-                tension: {
-                    duration: 1000,
-                    easing: 'linear',
-                    from: 1,
-                    to: 0,
-                    loop: true
-                }
-            },
-            scales: {
-                y: { // defining min and max so hiding the dataset does not change scale range
-                    min: 0,
-                    max: 20000
-                }
-            }
-
-        },
-
-    })
-
+// проверка localStorage, если пусто - создаем пустой массив
+if (localStorage.getItem('enteredSteps') === null) {
+    localStorage.setItem('enteredSteps', '[]');
 }
 
-// ничего не работает
+// создаем переменную с кнопкой, вешаем события по клику
+let btnMotionDiary = document.getElementById('btn_md');
+
+btnMotionDiary.addEventListener('click', saveInfoSteps);
+btnMotionDiary.addEventListener('click', stepsChartUpdate);
+
+// создаем функцию по сохранению данных
+function saveInfoSteps() {
+    const steps = document.getElementById('steps_today').value;
+    const stepsToday = document.getElementById('localdate').value;
+
+    // условие, при котором создается новый объект и пушатся доп.данные
+    if (steps && stepsToday) {
+        let enterStepsDay = new CounterS(stepsToday, steps)
+
+        let stepsArray = JSON.parse(localStorage.getItem('enteredSteps'))
+        stepsArray.push(enterStepsDay)
+
+        //   сортировка по датам (от меньшего к большему)  
+        stepsArray.sort(function (a, b) {
+            // Turn your strings into dates, and then subtract them
+            // to get a value that is either negative, positive, or zero.
+            return new Date(a.today) - new Date(b.today);
+        });
+
+        // преобразуем полученные данные из объекта в строку
+        let arrayForSave = JSON.stringify(stepsArray);
+// запись  в localStorage ключа и строки
+        localStorage.setItem('enteredSteps', arrayForSave)
+
+        console.log(stepsArray)
+        console.log(localStorage.getItem('enteredSteps'))
+
+    }
+    //     else {
+    //          создать див для ошибки и вывести, что не заполнено
+    // }
+}
 
 
-// function chartUpdate(){
-//     let labelsMotionDiary = arrDate;
-//     let dataMotionDiary = arrSteps;
-//     let colorsMotionDiary = ['#b1a28d'];
-    
-//     myChartMotionDiary.data.datasets=[{
-//         labels: labelsMotionDiary,
-//         label: 'Количество шагов',
-//         borderWidth: 2,
-//         fill: true,
-//         backgroundColor: '#bf9999',
-//         borderColor: '#fff',
-//         data: dataMotionDiary,
-//         pointRadius: 6,
-//         pointBackgroundColor: colorsMotionDiary
+// функция для записи в график даты(разделяем объект на две части для построения графика)
 
-//     }];
-//     myChartMotionDiary.update();
-// }
+function getArrayChartDate() {
 
+    let stepsArray = JSON.parse(localStorage.getItem('enteredSteps'))
+    if (stepsArray.length > null) {
+        arrDate = stepsArray.map(
+            function (object) {
+                return object.today
+
+            }
+        )
+        return arrDate
+    }
+}
+
+// функция для записи в график шагов
+
+function getArrayChartSteps() {
+
+    let stepsArray = JSON.parse(localStorage.getItem('enteredSteps'))
+    if (stepsArray.length > null) {
+        arrSteps = stepsArray.map(
+            function (object) {
+                return object.steps
+
+            }
+        )
+        return arrSteps
+    }
+}
+
+
+
+//     // график
+
+//  записываем в переменные функции, потому что не читает, если просто написать массивы
+
+let labelsMotionDiary = getArrayChartDate();
+let dataMotionDiary = getArrayChartSteps();
+let colorsMotionDiary = ['#b1a28d'];
+
+let myChartMotionDiary = document.getElementById("myChartMotionDiary").getContext('2d');
+
+myChartMotionDiary.canvas.width = 1200;
+myChartMotionDiary.canvas.height = 350;
+
+let chartMotionDiary = new Chart(myChartMotionDiary, {
+    type: 'line',
+    data: {
+        labels: labelsMotionDiary,
+        datasets: [{
+            label: 'Количество шагов',
+            borderWidth: 2,
+            fill: true,
+            backgroundColor: '#bf9999',
+            borderColor: '#fff',
+            data: dataMotionDiary,
+            pointRadius: 6,
+            pointBackgroundColor: colorsMotionDiary
+
+        }]
+    },
+    options: {
+
+        animations: {
+            tension: {
+                duration: 1000,
+                easing: 'linear',
+                from: 1,
+                to: 0,
+                loop: true
+            }
+        },
+        scales: {
+            y: { // defining min and max so hiding the dataset does not change scale range
+                min: 0,
+                max: 20000
+            }
+        }
+
+    },
+
+})
+
+
+
+// //тут  не работает, вопрос к datasets
+
+
+function stepsChartUpdate() {
+
+    let labelsMotionDiary = getArrayChartDate();
+    let dataMotionDiary = getArrayChartSteps();
+    let colorsMotionDiary = ['#b1a28d'];
+
+    myChartMotionDiary.data.datasets = [{
+        labels: getArrayChartDate(),
+        label: 'Количество шагов',
+        borderWidth: 2,
+        fill: true,
+        backgroundColor: '#bf9999',
+        borderColor: '#fff',
+        data: getArrayChartSteps(),
+        pointRadius: 6,
+        pointBackgroundColor: colorsMotionDiary
+
+    }];
+    myChartMotionDiary.update();
+}
+
+
+
+
+
+
+
+
+
+
+// график
+
+
+
+
+
+
+// let btnMotionDiary = document.getElementById('btn_md');
+// btnMotionDiary.addEventListener('click', saveInfoStep);
+// btnMotionDiary.addEventListener('click', stepsChartUpdate);
+
+
+
+// function saveInfo() {
+//     let stepsToday = document.getElementById('localdate').value;
+//     // console.log(stepsToday);
+//     let steps = document.getElementById('steps_today').value;
+//     // console.log(steps);
+
+
+//     arrDate.push(stepsToday);
+//     localStorage.setItem('stepsToday', JSON.stringify(arrDate));
+//     // console.log(arrDate);
+//     arrSteps.push(steps);
+//     localStorage.setItem('steps', JSON.stringify(arrSteps));
+//     // console.log(arrSteps);
 
 
 
