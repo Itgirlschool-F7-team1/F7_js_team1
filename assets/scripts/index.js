dd9327116a0d98bb50547c70bfd82a00ae8a94aa
Zoia Ponomareva <zojapon@yandex.ru>
2021-10-25 00:31:21 +0300

    Изменения в js

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,9 +1,10 @@
 const Chart = require('chart.js');
 
-commonModule = require('./takzdorovo_common');
+commonModule = require('./common/takzdorovo_common');
 // calctModule = require('./calories.js');
-randomRecipeModule = require('./random_recipe');
-myRecipesModule = require('./my_recipes');
-myRecipesModuleInput = require('./my_recipes/change_input')
+randomRecipeModule = require('./food_section/random_recipe/random_recipe');
+classRecipeModule = require('./food_section/my_recipes/class_Recipe')
+myRecipesModule = require('./food_section/my_recipes/my_recipes');
+myRecipesModuleInput = require('./food_section/my_recipes/change_input')
 // motionDiaryModule = require('./motion_diary');
 // IMTModule = require('./IMT');
